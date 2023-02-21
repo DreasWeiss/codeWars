3d4efe5ccd4405430739ff1c622b6aed0923cb61
DreasWeiss <dreasweiss@gmail.com>
2023-02-21 22:54:54 +0300

    Difference of Volumes of Cuboids

---
+++ katas/differenceOfVolumesOfCuboids/kata.js
@@ -0,0 +1,9 @@
+function findDifference(a, b) {
+    //loading...
+}
+
+console.log(findDifference([3, 2, 5], [1, 4, 4])) //, 14);
+console.log(findDifference([9, 7, 2], [5, 2, 2])) //, 106);
+console.log(findDifference([11, 2, 5], [1, 10, 8])) //, 30);
+console.log(findDifference([4, 4, 7], [3, 9, 3])) //, 31);
+console.log(findDifference([15, 20, 25], [10, 30, 25])) //, 0);
