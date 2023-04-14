16d4083c6dc6cbf9331c389e2a7871ae877f4d77
DreasWeiss <dreasweiss@gmail.com>
2023-04-14 20:39:53 +0300

    Opposite number - done

---
+++ katas/oppositeNumber/README.md
@@ -0,0 +1,14 @@
+# Opposite number
+8 kyu
+[link to kata](https://www.codewars.com/kata/56dec885c54a926dcd001095/train/javascript)
+<br>
+[my solution](./kata.js)
+
+Very simple, given an integer or a floating-point number, find its opposite.
+
+Examples:
+```
+1: -1
+14: -14
+-34: 34
+```
---
+++ katas/oppositeNumber/kata.js
@@ -0,0 +1,18 @@
+const opposite = n => n === 0 ? 0 : n * -1;
+
+console.log(opposite(1)); // -1
+
+
+console.log(opposite(0)); // 0
+
+
+console.log(opposite(4.25)); // -4.25
+
+
+console.log(opposite(3.3333333)); // -3.3333333
+
+
+console.log(opposite(-12525220.3325)); // 12525220.3325
+
+
+console.log(opposite(-5)); // 5
