d53d4361215800aa59aca3962e633a0cca647d7e
DreasWeiss <dreasweiss@gmail.com>
2023-04-24 17:27:36 +0300

    Disemvowel Trolls - done

---
+++ katas/disemvowelTrolls/kata.js
@@ -0,0 +1,16 @@
+function disemvowel(str) {
+    return str.replace(/a|e|u|i|o|A|E|U|I|O/g, '');
+}
+
+
+console.log(disemvowel("This website is for losers LOL!"));
+// "Ths wbst s fr lsrs LL!"
+
+
+console.log(disemvowel("No offense but,\nYour writing is among the worst I've ever read"));
+// "N ffns bt,\nYr wrtng s mng th wrst 'v vr rd"
+
+
+console.log(disemvowel("What are you, a communist?"));
+// "Wht r y,  cmmnst?"
+
