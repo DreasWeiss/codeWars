25ba3887cc58609ad7ce5932c9414aba02a9db87
DreasWeiss <dreasweiss@gmail.com>
2023-04-26 23:45:28 +0300

    Find Cracker. - done

--- katas/findCracker/kata.js
+++ katas/findCracker/kata.js
@@ -1,5 +1,29 @@
 function findHack(arr) {
-    return
+    const scores = {
+        'A': 30,
+        'B': 20,
+        'C': 10,
+        'D': 5
+    }
+    let answ = [];
+    for (let i = 0; i < arr.length; i++) {
+        let count = 0;
+        if (arr[i][2].length >= 5 &&
+            arr[i][2].length === arr[i][2]
+                .filter((s) => s === 'A' | s === 'B').length) {
+            count += 20;
+        }
+        arr[i][2].map((i) => {
+            if (scores.hasOwnProperty(i)) {
+                count += scores[i]
+            }
+        })
+        if (count !== arr[i][1]) {
+            answ.push(arr[i][0])
+        }
+    }
+
+    return answ;
 }
 
 var array = [
