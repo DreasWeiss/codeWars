e7cc6f69815febba1ed8b800a1ddd04ad3f487c1
DreasWeiss <dreasweiss@gmail.com>
2023-05-02 11:34:57 +0300

    Number of People in the Bus - done

--- katas/numberOfPeopleInTheBus/kata.js
+++ katas/numberOfPeopleInTheBus/kata.js
@@ -2,6 +2,10 @@ const number = function (busStops) {
     return busStops.reduce((acc, i) => acc + (i[0] - i[1]), 0);
 }
 
+// 
+// var number = function(busStops){
+//     return busStops.reduce((acc,[on, off]) => acc + on - off, 0);
+//   }
 
 console.log(number([[10, 0], [3, 5], [5, 8]])); //5
 console.log(number([[3, 0], [9, 1], [4, 10], [12, 2], [6, 1], [7, 10]])); //17
