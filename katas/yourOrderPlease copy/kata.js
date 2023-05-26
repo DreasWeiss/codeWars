ec7e72785770501033a9d897e54ee156bf12915f
DreasWeiss <dreasweiss@gmail.com>
2023-05-26 13:39:36 +0300

    Find Maximum and Minimum Values of a List - done

--- katas/yourOrderPlease copy/kata.js
+++ katas/yourOrderPlease copy/kata.js
@@ -1,12 +1,18 @@
-var min = function (list) {
+// #1
+// var min = function (list) {
 
-    return list[0];
-}
+//     return list.sort((a,b) => a - b)[0];
+// }
 
-var max = function (list) {
+// var max = function (list) {
 
-    return list[0];
-}
+//     return list.sort((a,b) => b - a)[0];
+// }
+
+
+//#2
+const min = (list) => Math.min(...list);
+const max = (list) => Math.max(...list);
 
 
 console.log(min([-52, 56, 30, 29, -54, 0, -110])); // -110
