9dd79aab152fd8d99ac44b4f297936a12f9643a8
DreasWeiss <dreasweiss@gmail.com>
2023-06-07 20:10:05 +0300

    Multiples of 3 or 5 - done

--- katas/multiplesOf3Or5/kata.js
+++ katas/multiplesOf3Or5/kata.js
@@ -1,11 +1,36 @@
-function solution(number) {
-    let counter = 0;
-    let i = 0;
+// #1
+// function solution(number) {
+//     let counter = 0;
+//     let i = 0;
 
-    while (i < number) {
+//     while (i < number) {
+//         if (i%3===0 || i%5===0) {
+//             counter+=i;
+//         }
+//         i++
+//     }
+//     return counter;
+// }
 
-    }
-}
 
+// #2
+// function solution(number){
+//   let total = 0;
+
+//   for (let i=0; i<number; i++) {
+//     if (i % 3 === 0 || i % 5 ===0){
+//       total += i;
+//     }
+//   }
+//   return total;
+// }
+
+
+// #3
+// const solution = n => n < 1 ? 0 : [...new Array(n).keys()].reduce((acc, i) => acc + (i%3===0 || i%5===0 ? i : 0));
+
+
+//#4
+const solution = n => n <= 0 ? 0 : Array.from({ length: n }, (_, i) => (i % 3 == 0 || i % 5 == 0 ? i : 0)).reduce((a, b) => a + b);
 
 console.log(solution(10)); // 23
