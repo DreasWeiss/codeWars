faf0774c7f41f830069420b1f4fc054059ef3979
DreasWeiss <dreasweiss@gmail.com>
2023-10-26 22:33:32 +0300

    Uglify Word - done

--- katas/uglifyWord/kata.js
+++ katas/uglifyWord/kata.js
@@ -1,23 +1,27 @@
-function uglify_word(s) {
-    // your code here
-    s = s.toLowerCase().split("");
-    let flag = 1;
-    for (let i = 0; i < s.length; i++) {
-        if (/[a-z]/.test(s[i])) {
-            if (flag === 1) {
-                let str = s[i].toUpperCase();
-                s[i] = str;
-                flag = 0;
-            } else {
-                let str = s[i].toLowerCase();
-                s[i] = str;
-                flag = 1;
-            }
-        } else {
-            flag = 1;
-        }
-    }
-    return s.join(``);
+// function uglify_word(s) {
+//     // your code here
+//     s = s.toLowerCase().split("");
+//     let flag = 1;
+//     for (let i = 0; i < s.length; i++) {
+//         if (/[a-z]/.test(s[i])) {
+//             if (flag === 1) {
+//                 let str = s[i].toUpperCase();
+//                 s[i] = str;
+//                 flag = 0;
+//             } else {
+//                 let str = s[i].toLowerCase();
+//                 s[i] = str;
+//                 flag = 1;
+//             }
+//         } else {
+//             flag = 1;
+//         }
+//     }
+//     return s.join(``);
+// }
+
+function uglifyWord(s) {
+    return s.replace(/[a-z]+/ig, x => x.split``.map((y, i) => i % 2 ? y.toLowerCase() : y.toUpperCase()).join``)
 }
 
 
