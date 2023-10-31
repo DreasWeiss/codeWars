bc936c8740cf9088edffe0b711774d7a2e56cd45
DreasWeiss <dreasweiss@gmail.com>
2023-10-31 23:57:31 +0300

    Find Count of Most Frequent Item in an Array - done

---
+++ katas/yourOrderPlease copy/kata.js
@@ -0,0 +1,10 @@
+function mostFrequentItemCount(collection) {
+    // Do your magic. :)
+}
+
+
+console.log(mostFrequentItemCount([3, -1, -1]));
+// 2
+
+console.log(mostFrequentItemCount([3, -1, -1, -1, 2, 3, -1, 3, -1, 2, 4, 9, 3]));
+// 5
