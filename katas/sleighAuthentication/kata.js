3c40cd1264f81243103b7659d9e82e0a19df2153
Dreas Weiss <dreasweiss@gmail.com>
2024-04-12 21:21:15 +0300

    Sleigh Authentication - done

--- katas/sleighAuthentication/kata.js
+++ katas/sleighAuthentication/kata.js
@@ -9,11 +9,21 @@
 // };
 
 // #2
-function Sleigh() {
-    this.name = "Santa Claus";
-    this.password = "Ho Ho Ho!";
-}
+// function Sleigh() {
+//     this.name = "Santa Claus";
+//     this.password = "Ho Ho Ho!";
+// }
   
-Sleigh.prototype.authenticate = function(name, password) {
-    return this.name == name && this.password == password;
-};
+// Sleigh.prototype.authenticate = function(name, password) {
+//     return this.name == name && this.password == password;
+// };
+
+// #3
+class Sleigh {
+    constructor() {
+      this.obj = {'Santa Claus': `Ho Ho Ho!`};
+    }
+    authenticate(name, password) {
+      return this.obj[name] === password;
+    }
+}
