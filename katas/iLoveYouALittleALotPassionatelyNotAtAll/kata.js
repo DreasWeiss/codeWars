20cbdf6ab177c01f99a6f6635c9a80800fcff004
Dreas Weiss <dreasweiss@gmail.com>
2024-08-20 16:56:16 +0200

    I love you, a little , a lot, passionately ... not at all - done

--- katas/iLoveYouALittleALotPassionatelyNotAtAll/kata.js
+++ katas/iLoveYouALittleALotPassionatelyNotAtAll/kata.js
@@ -1,15 +1,35 @@
-function howMuchILoveYou(nbPetals) {
-  // your code
-  let l = [
-    'not at all',
-    'I love you',
-    'a little',
-    'a lot',
-    'passionately',
-    'madly',
-  ];
+// #1
+// function howMuchILoveYou(nbPetals) {
+//   // your code
+//   let l = [
+//     'not at all',
+//     'I love you',
+//     'a little',
+//     'a lot',
+//     'passionately',
+//     'madly',
+//   ];
 
-  return l[nbPetals % 6];
+//   return l[nbPetals % 6];
+// }
+
+
+// #2
+function howMuchILoveYou(nbPetals) {
+  switch ((nbPetals - 1) % 6) {
+    case 1:
+      return 'a little';
+    case 2:
+      return 'a lot';
+    case 3:
+      return 'passionately';
+    case 4:
+      return 'madly';
+    case 5:
+      return 'not at all';
+    default:
+      return 'I love you';
+  }
 }
 
 console.log(howMuchILoveYou(128)); // 'a little'
